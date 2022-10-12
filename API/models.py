14af0d9219e6fc9fd7119e6b7bd76497d4a4c89f
Shumma <102603818+MrShummaUC@users.noreply.github.com>
2022-10-12 19:42:08 -0300

    Nueva BD y población models.py

--- API/models.py
+++ API/models.py
@@ -2,23 +2,26 @@
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
-    #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Asignatura(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=20)
     nombre = models.CharField(max_length=100)
+    id_asig = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=10)
 
     class Meta:
         managed = False
         db_table = 'asignatura'
 
 class Curso(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=20)
     nombre = models.CharField(max_length=100)
+    id_curso = models.AutoField(primary_key=True)
+    secciones = models.CharField(max_length=100)
+    id_asig = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -32,11 +35,11 @@ class Estudiante(models.Model):
     apmaterno = models.CharField(max_length=20)
     user = models.CharField(max_length=30)
     contrasena = models.CharField(max_length=30)
-    
+    id_tipo = models.ForeignKey('TipoUser', models.DO_NOTHING, db_column='id_tipo', blank=True, null=True)
+
     class Meta:
         managed = False
         db_table = 'estudiante'
-
 
 class Profesor(models.Model):
     run = models.IntegerField(primary_key=True)
@@ -46,14 +49,14 @@ class Profesor(models.Model):
     apmaterno = models.CharField(max_length=20)
     user = models.CharField(max_length=30)
     contrasena = models.CharField(max_length=30)
+    id_tipo = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'profesor'
 
-
 class TipoUser(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id_tipo = models.AutoField(primary_key=True)
     tipo = models.CharField(max_length=30)
 
     class Meta:
---
+++ db_qr_app.sql
@@ -0,0 +1,600 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-10-2022 a las 00:36:21
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_qr_app`
+--
+CREATE DATABASE IF NOT EXISTS `db_qr_app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_qr_app`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `nombre` varchar(100) NOT NULL,
+  `id_asig` int(11) NOT NULL,
+  `codigo` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`nombre`, `id_asig`, `codigo`) VALUES
+('ESTADISTICA DESCRIPTIVA', 1, 'MAT4140'),
+('PROGRAMACION DE APLICACIONES MOVILES', 2, 'PGY4121');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_group`
+--
+
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_group_permissions`
+--
+
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint(20) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_permission`
+--
+
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `auth_permission`
+--
+
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+(1, 'Can add log entry', 1, 'add_logentry'),
+(2, 'Can change log entry', 1, 'change_logentry'),
+(3, 'Can delete log entry', 1, 'delete_logentry'),
+(4, 'Can view log entry', 1, 'view_logentry'),
+(5, 'Can add permission', 2, 'add_permission'),
+(6, 'Can change permission', 2, 'change_permission'),
+(7, 'Can delete permission', 2, 'delete_permission'),
+(8, 'Can view permission', 2, 'view_permission'),
+(9, 'Can add group', 3, 'add_group'),
+(10, 'Can change group', 3, 'change_group'),
+(11, 'Can delete group', 3, 'delete_group'),
+(12, 'Can view group', 3, 'view_group'),
+(13, 'Can add user', 4, 'add_user'),
+(14, 'Can change user', 4, 'change_user'),
+(15, 'Can delete user', 4, 'delete_user'),
+(16, 'Can view user', 4, 'view_user'),
+(17, 'Can add content type', 5, 'add_contenttype'),
+(18, 'Can change content type', 5, 'change_contenttype'),
+(19, 'Can delete content type', 5, 'delete_contenttype'),
+(20, 'Can view content type', 5, 'view_contenttype'),
+(21, 'Can add session', 6, 'add_session'),
+(22, 'Can change session', 6, 'change_session'),
+(23, 'Can delete session', 6, 'delete_session'),
+(24, 'Can view session', 6, 'view_session'),
+(25, 'Can add asignatura', 7, 'add_asignatura'),
+(26, 'Can change asignatura', 7, 'change_asignatura'),
+(27, 'Can delete asignatura', 7, 'delete_asignatura'),
+(28, 'Can view asignatura', 7, 'view_asignatura'),
+(29, 'Can add curso', 8, 'add_curso'),
+(30, 'Can change curso', 8, 'change_curso'),
+(31, 'Can delete curso', 8, 'delete_curso'),
+(32, 'Can view curso', 8, 'view_curso'),
+(33, 'Can add estudiante', 9, 'add_estudiante'),
+(34, 'Can change estudiante', 9, 'change_estudiante'),
+(35, 'Can delete estudiante', 9, 'delete_estudiante'),
+(36, 'Can view estudiante', 9, 'view_estudiante'),
+(37, 'Can add profesor', 10, 'add_profesor'),
+(38, 'Can change profesor', 10, 'change_profesor'),
+(39, 'Can delete profesor', 10, 'delete_profesor'),
+(40, 'Can view profesor', 10, 'view_profesor'),
+(41, 'Can add tipo user', 11, 'add_tipouser'),
+(42, 'Can change tipo user', 11, 'change_tipouser'),
+(43, 'Can delete tipo user', 11, 'delete_tipouser'),
+(44, 'Can view tipo user', 11, 'view_tipouser');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_user`
+--
+
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_user_groups`
+--
+
+CREATE TABLE `auth_user_groups` (
+  `id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_user_user_permissions`
+--
+
+CREATE TABLE `auth_user_user_permissions` (
+  `id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `nombre` varchar(100) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `secciones` varchar(100) NOT NULL,
+  `id_asig` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`nombre`, `id_curso`, `secciones`, `id_asig`) VALUES
+('PROGRAMACION DE APLICACIONES MOVILES', 1, '001D', 1),
+('PROGRAMACION DE APLICACIONES MOVILES', 2, '002D', 1),
+('PROGRAMACION DE APLICACIONES MOVILES', 3, '003D', 1),
+('ESTADISTICA DESCRIPTIVA', 4, '004D', 2),
+('ESTADISTICA DESCRIPTIVA', 5, '005D', 2),
+('ESTADISTICA DESCRIPTIVA', 6, '006D', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `django_admin_log`
+--
+
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext DEFAULT NULL,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) UNSIGNED NOT NULL CHECK (`action_flag` >= 0),
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `django_content_type`
+--
+
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `django_content_type`
+--
+
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+(1, 'admin', 'logentry'),
+(7, 'API', 'asignatura'),
+(8, 'API', 'curso'),
+(9, 'API', 'estudiante'),
+(10, 'API', 'profesor'),
+(11, 'API', 'tipouser'),
+(3, 'auth', 'group'),
+(2, 'auth', 'permission'),
+(4, 'auth', 'user'),
+(5, 'contenttypes', 'contenttype'),
+(6, 'sessions', 'session');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `django_migrations`
+--
+
+CREATE TABLE `django_migrations` (
+  `id` bigint(20) NOT NULL,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `django_migrations`
+--
+
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+(1, 'contenttypes', '0001_initial', '2022-10-01 02:11:56.944689'),
+(2, 'auth', '0001_initial', '2022-10-01 02:11:57.201878'),
+(3, 'admin', '0001_initial', '2022-10-01 02:11:57.267017'),
+(4, 'admin', '0002_logentry_remove_auto_add', '2022-10-01 02:11:57.272005'),
+(5, 'admin', '0003_logentry_add_action_flag_choices', '2022-10-01 02:11:57.278304'),
+(6, 'contenttypes', '0002_remove_content_type_name', '2022-10-01 02:11:57.321583'),
+(7, 'auth', '0002_alter_permission_name_max_length', '2022-10-01 02:11:57.350781'),
+(8, 'auth', '0003_alter_user_email_max_length', '2022-10-01 02:11:57.367594'),
+(9, 'auth', '0004_alter_user_username_opts', '2022-10-01 02:11:57.375073'),
+(10, 'auth', '0005_alter_user_last_login_null', '2022-10-01 02:11:57.406391'),
+(11, 'auth', '0006_require_contenttypes_0002', '2022-10-01 02:11:57.408384'),
+(12, 'auth', '0007_alter_validators_add_error_messages', '2022-10-01 02:11:57.413368'),
+(13, 'auth', '0008_alter_user_username_max_length', '2022-10-01 02:11:57.427804'),
+(14, 'auth', '0009_alter_user_last_name_max_length', '2022-10-01 02:11:57.438861'),
+(15, 'auth', '0010_alter_group_name_max_length', '2022-10-01 02:11:57.452739'),
+(16, 'auth', '0011_update_proxy_permissions', '2022-10-01 02:11:57.458719'),
+(17, 'auth', '0012_alter_user_first_name_max_length', '2022-10-01 02:11:57.470668'),
+(18, 'sessions', '0001_initial', '2022-10-01 02:11:57.491626'),
+(19, 'API', '0001_initial', '2022-10-12 01:25:51.029475');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `django_session`
+--
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `run` int(8) NOT NULL,
+  `dv` int(1) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `appaterno` varchar(20) NOT NULL,
+  `apmaterno` varchar(20) NOT NULL,
+  `user` varchar(30) NOT NULL,
+  `contrasena` varchar(30) NOT NULL,
+  `id_tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`run`, `dv`, `nombre`, `appaterno`, `apmaterno`, `user`, `contrasena`, `id_tipo`) VALUES
+(20906454, 5, 'David', 'Martinez', 'Manciet', 'dav.martinez', '20906454', 2),
+(21189889, 5, 'Javier', 'Inostroza', 'Marambio', 'ja.inostroza', '21189889', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesor`
+--
+
+CREATE TABLE `profesor` (
+  `run` int(8) NOT NULL,
+  `dv` int(1) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `appaterno` varchar(20) NOT NULL,
+  `apmaterno` varchar(20) NOT NULL,
+  `user` varchar(30) NOT NULL,
+  `contrasena` varchar(30) NOT NULL,
+  `id_tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `profesor`
+--
+
+INSERT INTO `profesor` (`run`, `dv`, `nombre`, `appaterno`, `apmaterno`, `user`, `contrasena`, `id_tipo`) VALUES
+(11111111, 1, 'FREDDY', 'CAMPOS', 'ORELLANA', 'fre.camposo', '11111111', 3),
+(22222222, 2, 'JUAN', 'BARRERA', 'LABRA', 'ju.barrera', '22222222', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_user`
+--
+
+CREATE TABLE `tipo_user` (
+  `id_tipo` int(11) NOT NULL,
+  `tipo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipo_user`
+--
+
+INSERT INTO `tipo_user` (`id_tipo`, `tipo`) VALUES
+(1, 'Admin'),
+(2, 'Alumno'),
+(3, 'Profesor');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id_asig`);
+
+--
+-- Indices de la tabla `auth_group`
+--
+ALTER TABLE `auth_group`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indices de la tabla `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  ADD KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`);
+
+--
+-- Indices de la tabla `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`);
+
+--
+-- Indices de la tabla `auth_user`
+--
+ALTER TABLE `auth_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indices de la tabla `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  ADD KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`);
+
+--
+-- Indices de la tabla `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  ADD KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`),
+  ADD KEY `id_asig` (`id_asig`);
+
+--
+-- Indices de la tabla `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  ADD KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`);
+
+--
+-- Indices de la tabla `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`);
+
+--
+-- Indices de la tabla `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `django_session`
+--
+ALTER TABLE `django_session`
+  ADD PRIMARY KEY (`session_key`),
+  ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`run`),
+  ADD KEY `id_tipo` (`id_tipo`) USING BTREE;
+
+--
+-- Indices de la tabla `profesor`
+--
+ALTER TABLE `profesor`
+  ADD PRIMARY KEY (`run`),
+  ADD KEY `id_tipo` (`id_tipo`);
+
+--
+-- Indices de la tabla `tipo_user`
+--
+ALTER TABLE `tipo_user`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id_asig` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_group`
+--
+ALTER TABLE `auth_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_user`
+--
+ALTER TABLE `auth_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_user`
+--
+ALTER TABLE `tipo_user`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`);
+
+--
+-- Filtros para la tabla `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Filtros para la tabla `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  ADD CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Filtros para la tabla `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Filtros para la tabla `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  ADD CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo_user` (`id_tipo`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
