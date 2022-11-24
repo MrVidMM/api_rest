eca9c4fec81a90e6ac89c8d0ddb7a70eb0f75911
MrVid <102604076+MrVidMM@users.noreply.github.com>
2022-11-24 09:34:19 -0300

    politicas cors desabilitadas

--- API/urls.py
+++ API/urls.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
 from django.urls import re_path
 from API import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
-        re_path(r'^api/Usuarios/$',views.UsuariosViewSet.as_view()),
-        re_path(r'^api/Usuarios/(?P<user>.+)/(?P<contrasena>.+)/$',views.UsuarioSesionViewSet.as_view()),
-        re_path(r'^api/TipoUser/$',views.TipoUserViewSet.as_view()),
-        re_path(r'^api/TipoUser/(?P<id_tipo>.+)/$',views.TipoUserBuscarViewSet.as_view()),
-        re_path(r'^api/Asignaturas/$',views.AsignaturasViewSet.as_view()),
-        re_path(r'^api/AsignaturasRun/(?P<run>.+)/$',views.AsignaturasProfesorViewSet.as_view()),
-        re_path(r'^api/Asignatura/(?P<id_asig>.+)/$',views.AsignaturasBuscarViewSet.as_view()),
-        re_path(r'^api/Jornada/$',views.JornadaViewSet.as_view()),
-        re_path(r'^api/Jornada/(?P<id_jornada>.+)/$',views.JornadaBuscarViewSet.as_view()),
-        re_path(r'^api/JornadaAsignatura/(?P<id_asig>.+)/$',views.JornadaAsignatura.as_view()),
-        re_path(r'^api/Secciones/$',views.SeccionesViewSet.as_view()),
-        re_path(r'^api/Secciones/(?P<id_jornada>.+)/$',views.SeccionesJornadaViewSet.as_view()),
-        re_path(r'^api/Asistencia/$',views.AsistenciaViewSet.as_view()),
-        re_path(r'^api/Asistencia/(?P<run>.+)/$',views.AsistenciaBuscarViewSet.as_view()),
+        re_path(r'^api/Usuarios/$',csrf_exempt(views.UsuariosViewSet.as_view())),
+        re_path(r'^api/Usuarios/(?P<user>.+)/(?P<contrasena>.+)/$',csrf_exempt(views.UsuarioSesionViewSet.as_view())),
+        re_path(r'^api/TipoUser/$',csrf_exempt(views.TipoUserViewSet.as_view())),
+        re_path(r'^api/TipoUser/(?P<id_tipo>.+)/$',csrf_exempt(views.TipoUserBuscarViewSet.as_view())),
+        re_path(r'^api/Asignaturas/$',csrf_exempt(views.AsignaturasViewSet.as_view())),
+        re_path(r'^api/AsignaturasRun/(?P<run>.+)/$',csrf_exempt(views.AsignaturasProfesorViewSet.as_view())),
+        re_path(r'^api/Asignatura/(?P<id_asig>.+)/$',csrf_exempt(views.AsignaturasBuscarViewSet.as_view())),
+        re_path(r'^api/Jornada/$',csrf_exempt(views.JornadaViewSet.as_view())),
+        re_path(r'^api/Jornada/(?P<id_jornada>.+)/$',csrf_exempt(views.JornadaBuscarViewSet.as_view())),
+        re_path(r'^api/JornadaAsignatura/(?P<id_asig>.+)/$',csrf_exempt(views.JornadaAsignatura.as_view())),
+        re_path(r'^api/Secciones/$',csrf_exempt(views.SeccionesViewSet.as_view())),
+        re_path(r'^api/Secciones/(?P<id_jornada>.+)/$',csrf_exempt(views.SeccionesJornadaViewSet.as_view())),
+        re_path(r'^api/Asistencia/$',csrf_exempt(views.AsistenciaViewSet.as_view())),
+        re_path(r'^api/Asistencia/(?P<run>.+)/$',csrf_exempt(views.AsistenciaBuscarViewSet.as_view())),
         
     ]
+
+urlpatterns=format_suffix_patterns(urlpatterns)
     
