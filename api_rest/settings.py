1a091b36479c8a3cfea229372b01f304f9feaa92
MrVid <102604076+MrVidMM@users.noreply.github.com>
2022-11-24 09:04:57 -0300

    Agregado cors nuevamente

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'API',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.common.CommonMiddleware",
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
