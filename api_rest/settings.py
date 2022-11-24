d9cfa15842d0defbde7479aa772d976654e0d514
MrVid <102604076+MrVidMM@users.noreply.github.com>
2022-11-24 09:43:46 -0300

    Agregado acceso metodos cors

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -29,6 +29,15 @@ DEBUG = False
 CORS_ALLOWED_ORIGINS = ['*']
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -138,3 +147,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
